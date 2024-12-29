c733ef131082ee20d8a90e9301367c0000b55ac8
Dimon <kachalin_dmitriy@mail.ru>
2024-12-29 12:28:49 +0300

    Этап 1: Задание 3
    сделал команду input к датам, хотя это немного странно. На мой взгляд дату создания программа должна взять и вывести самостоятельно, без пользователя, в момент сохранения заметки. А вот дату истечения "срока годности" заявки - пользователь уже может вписать - а еще лучше выбрать из выпадающего списка, как у большинства подобных приложений и сделано - это удобно. )

---
+++ add_input.py
@@ -0,0 +1,18 @@
+username = input('Введите Ваше имя: ')
+title = input('Введите заголовок заметки: ')
+content = input('Введите описание заметки: ')
+status = input('Введите статус заметки: ')
+created_date = input('Введите дату создания заметки, в формате dd.mm.yyyy: ')
+issue_date = input('Введите дату истечения срока заметки, в формате dd.mm.yyyy: ')
+print(f'Имя пользователя: {username}')
+print(f'Заголовок заметки: {title}')
+print(f'Описание заметки: {content}')
+print(f'Статус заметки: {status}')
+print(f'Дата создания: {created_date}')
+print(f'Дата истечения заметки (дедлайн): {issue_date}')
+
+
+
+
+
+
