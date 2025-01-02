c8cb89bd8f1d84a957b759656adae53a24ab504b
Dimon <kachalin_dmitriy@mail.ru>
2025-01-02 19:39:29 +0300

    Этап 1: Задание 2

--- date_changer.py
+++ date_changer.py
@@ -1,6 +1,13 @@
-created_date = ('28.12.2024')
-print(created_date[0:6])
-issue_date = ('28.01.2025')
-print(issue_date[0:6])
+username = input('Введите Ваше имя: ')
+title = input('Введите заголовок заметки: ')
+content = input('Введите описание заметки: ')
+status = input('Введите статус заметки: ')
+created_date = input('Введите дату создания заметки, в формате dd.mm.yyyy: ')
+issue_date = input('Введите дату истечения срока заметки, в формате dd.mm.yyyy: ')
 
-
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print(f"Дата создания заметки: {created_date[:6]}")
+print(f"Дата истечения заметки: {issue_date[:6]}")
