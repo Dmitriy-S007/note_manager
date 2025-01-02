12969abb44a13bed8d367595382ed0c1ef5b7943
Dimon <kachalin_dmitriy@mail.ru>
2025-01-02 19:55:01 +0300

    Этап 1: Задание 3

--- add_input.py
+++ add_input.py
@@ -1,17 +1,16 @@
-username = input('Введите Ваше имя: ')
-title = input('Введите заголовок заметки: ')
-content = input('Введите описание заметки: ')
-status = input('Введите статус заметки: ')
-created_date = input('Введите дату создания заметки, в формате dd.mm.yyyy: ')
-issue_date = input('Введите дату истечения срока заметки, в формате dd.mm.yyyy: ')
-print(f'Имя пользователя: {username}')
-print(f'Заголовок заметки: {title}')
-print(f'Описание заметки: {content}')
-print(f'Статус заметки: {status}')
-print(f'Дата создания: {created_date}')
-print(f'Дата истечения заметки (дедлайн): {issue_date}')
+username = input("Введите имя пользователя: ")
+title = input("Введите заголовок заметки: ")
+content = input("Введите описание заметки: ")
+status = input("Введите статус заметки: ")
+created_date = input("Введите дату создания заметки в формате ДД.ММ.ГГГГ: ")
+issue_date = input("Введите дату истечения заметки в формате ДД.ММ.ГГГГ: ")
 
-
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date[:6])
+print("Дата истечения заметки:", issue_date[:6])
 
 
 
