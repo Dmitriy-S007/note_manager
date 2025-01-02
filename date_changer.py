fe03fcd2f81cff63efa21583cc9d38786e24611c
Dimon <kachalin_dmitriy@mail.ru>
2025-01-02 19:51:25 +0300

    Этап 1: Задание 2

--- date_changer.py
+++ date_changer.py
@@ -1,16 +1,13 @@
-username = input('Введите Ваше имя: ')
-title = input('Введите заголовок заметки: ')
-content = input('Введите описание заметки: ')
-status = input('Введите статус заметки: ')
-created_date = input('Введите дату создания заметки, в формате dd.mm.yyyy: ')
-issue_date = input('Введите дату истечения срока заметки, в формате dd.mm.yyyy: ')
-
-temp_created_date = created_date[:6] # Создаем временную переменную
-temp_issue_date = issue_date[:6] # Создаем временную переменную
+username = 'Введите Ваше имя: '
+title = 'Введите заголовок заметки: '
+content = 'Введите описание заметки: '
+status = 'Введите статус заметки: '
+created_date ='Введите дату создания заметки, в формате dd.mm.yyyy: '
+issue_date = 'Введите дату истечения срока заметки, в формате dd.mm.yyyy: '
 
 print("Имя пользователя:", username)
 print("Заголовок заметки:", title)
 print("Описание заметки:", content)
 print("Статус заметки:", status)
-print("Дата создания заметки:", temp_created_date)
-print("Дата истечения заметки:", temp_issue_date)
+print("Дата создания заметки:", created_date[:5])
+print("Дата истечения заметки:", issue_date[:5])
