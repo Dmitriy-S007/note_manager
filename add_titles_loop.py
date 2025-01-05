e95164284e1bc10da36fb23e01e1f8afa74a689a
Dimon <kachalin_dmitriy@mail.ru>
2025-01-05 13:38:01 +0300

    Этап 2: Задание 1

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,3 +1,8 @@
+note = []
+
+username = input('Введите Ваше имя: ')
+note.append(username)
+
 titles = set()
 
 while True:
@@ -11,6 +16,25 @@ while True:
     else:
         break
 
+note.append(title)
+
+content = input('Введите описание заметки: ')
+note.append(content)
+
+status = input('Введите статус заметки: ')
+note.append(status)
+
+created_date = input('Введите дату создания заметки, в формате dd.mm.yyyy: ')
+note.append(created_date)
+
+issue_date = input('Введите дату истечения срока заметки, в формате dd.mm.yyyy: ')
+note.append(issue_date)
+
+print("Имя пользователя:", note[0])
+print("Содержание заметки:", note[2])
+print("Статус:", note[3])
+print("Дата создания заметки:", note [4][:6])
+print("Дата истечения заметки:", note [5][:6])
 print("\nЗаголовки заметки:")
 for title in titles:
     print(f"- {title}")
